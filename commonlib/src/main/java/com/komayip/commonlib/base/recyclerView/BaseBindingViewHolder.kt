47c949adf95d69d1eb82b0f3b9409c8a69fa97bc
Koma Yip <komayip@hkasianark.com>
2020-07-10 09:55:12 +0800

    0.0.13

--- commonlib/src/main/java/com/komayip/commonlib/base/recyclerView/BaseBindingViewHolder.kt
+++ commonlib/src/main/java/com/komayip/commonlib/base/recyclerView/BaseBindingViewHolder.kt
@@ -2,16 +2,24 @@ package com.komayip.commonlib.base.recyclerView
 
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.disposables.CompositeDisposable
 
 /**
  * Abstract RecyclerViewHolder For Data Binding
  */
 abstract class BaseBindingViewHolder<T>(open val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    val disposeBag = CompositeDisposable()
+
     /**
      *  bind view model here
      */
     abstract fun bind(data: T)
+
+    /**
+     * add th bind event here, onclick or rxjava, which should be something view holder independent
+     */
+    abstract fun bindEvent()
 
     /**
      *  execute in onBindViewHolder
