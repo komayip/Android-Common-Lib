f04a5144ddee93ed53520952fb499fdc1a49e262
Koma Yip <komayip@hkasianark.com>
2020-09-09 15:19:45 +0800

    0.0.20

---
+++ commonlib/src/main/java/com/komayip/commonlib/ext/FragmentTransactionExt.kt
@@ -0,0 +1,8 @@
+package com.komayip.commonlib.ext
+
+import androidx.fragment.app.FragmentTransaction
+import com.komayip.commonlib.R
+
+fun FragmentTransaction.slide() {
+    this.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
+}
